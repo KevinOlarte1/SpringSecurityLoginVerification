a191b2a07a71704af0392d19e4e0cc290b176f76
kevin <kevinolarte.ko@gmail.com>
2025-03-27 15:06:10 +0100

    Añadir el la auth para endPoinst.
    -Hemos creado el primer endPoint publico para verificar que funciona
    bien todo.
    
    -El password ecoder hemos creado un metodo que sera el encargado de
    codificar.
    
    -Ya hemos puesto filteracion a los endpPoints. falta que sea el nuestro
    correcto eso mas adelante.

--- src/main/java/com/kevinolarte/demo/registration/RegistrationController.java
+++ src/main/java/com/kevinolarte/demo/registration/RegistrationController.java
@@ -3,6 +3,7 @@ package com.kevinolarte.demo.registration;
 import com.kevinolarte.demo.appuser.AppUser;
 import lombok.AllArgsConstructor;
 import org.springframework.security.web.webauthn.management.ImmutableRelyingPartyRegistrationRequest;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +15,11 @@ public class RegistrationController {
 
     private RegistrationService registrationService;
 
+    @PostMapping
     public String register(@RequestBody RegistrationRequest request) {
+        //TODO: Verificar que el email.
+        //VIDEO: Java Tutorial - Complete User Login and Registration Backend + Email Verification
+        //MINUTO: 46:50.
         return registrationService.register(request);
     }
 }
--- src/main/java/com/kevinolarte/demo/security/config/WebSecurityConfig.java
+++ src/main/java/com/kevinolarte/demo/security/config/WebSecurityConfig.java
@@ -1,4 +1,54 @@
 package com.kevinolarte.demo.security.config;
 
+import com.kevinolarte.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@AllArgsConstructor
+@EnableWebSecurity
 public class WebSecurityConfig {
+
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/v*/registration/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    protected AuthenticationManager authenticationManager(AuthenticationConfiguration config)
+            throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
+    }
 }
